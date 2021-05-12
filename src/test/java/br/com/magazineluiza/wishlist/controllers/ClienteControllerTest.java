58717933be84216939d4cedcf8f84ec699c62033
Luanna <luanna.rodrigues@hotmail.com>
2021-05-12 18:51:47 -0300

    adjusted test clientController

--- src/test/java/br/com/magazineluiza/wishlist/controllers/ClienteControllerTest.java
+++ src/test/java/br/com/magazineluiza/wishlist/controllers/ClienteControllerTest.java
@@ -9,12 +9,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -25,6 +23,7 @@ import br.com.magazineluiza.wishlist.domain.entity.ClienteBuilder;
 import br.com.magazineluiza.wishlist.controllers.ClienteController;
 import br.com.magazineluiza.wishlist.domain.service.ClienteService;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -42,20 +41,13 @@ public class ClienteControllerTest {
     @DisplayName("Test createShouldReturnCliente Success")
 	public void createShouldReturnCliente() {
         Cliente cliente = new ClienteBuilder().defaultValues();
-            // when(service.Create(cliente)).thenReturn(cliente);
-
-        // this.mockMvc.perform(MockMvcRequestBuilders.post("/cliente")
-        //           .contentType(MediaType.APPLICATION_JSON_VALUE)
-        //           .content(asJsonString(cliente)))
-        //           .andExpect(MockMvcResultMatchers.status().isOk());
+        
         when(service.Create(cliente)).thenReturn(cliente);
+        Cliente result = clienteController.Create(cliente);
  
-        ResponseEntity<Cliente> result = clienteController.Create(cliente);
- 
-        assertEquals(HttpStatus.OK.value(), result.getStatusCode().value());
-        assertEquals("nome", result.getBody().getNome());
-        assertEquals("sobrenome", result.getBody().getSobrenome());
-        assertEquals("cpf", result.getBody().getc());
+        assertEquals("Joao", result.getNome());
+        assertEquals("Ferreira", result.getSobrenome());
+        assertEquals("64195160057", result.getCpf());
 	}
 
   
