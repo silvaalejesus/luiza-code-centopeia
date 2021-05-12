6028933a568aed836e117af9a38fd9c029a61458
anapaulamarce <anapmarcelino98@gmail.com>
2021-05-11 23:03:02 -0300

    Iniciando WishListService, alterando WishListController e adicionando o DER

--- src/main/java/br/com/magazineluiza/wishlist/controllers/WishListController.java
+++ src/main/java/br/com/magazineluiza/wishlist/controllers/WishListController.java
@@ -1,5 +1,14 @@
 package br.com.magazineluiza.wishlist.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.magazineluiza.wishlist.domain.entity.Produto;
+import br.com.magazineluiza.wishlist.domain.entity.WishList;
+import br.com.magazineluiza.wishlist.domain.service.ProdutoService;
 
 @RestController
 @RequestMapping("/adicionar-produto")
@@ -7,11 +16,18 @@ public class WishListController {
     @Autowired
     private ProdutoService _produtoService;
 
+    @Autowired
+    private WishListService _WishListService;
+    
     @RequestMapping(method = RequestMethod.POST)
     public Produto Create(@RequestBody Produto produto) {
         return _produtoService.Create(produto);
-        
     }
-}
-    
+
+
+    @RequestMapping(method = RequestMethod.POST)
+    public WishList Create(@RequestBody WishList WishList) {
+        return _WishListService.Create(WishList);    
+           
+    }
 }
