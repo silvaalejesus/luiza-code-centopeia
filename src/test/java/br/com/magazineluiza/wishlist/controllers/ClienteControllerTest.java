bea9f09c6dffadb7798e4d1dd0328020a71ce88c
Vanessa Santana <vanessah4money@gmail.com>
2021-05-12 07:57:45 -0300

    test: criando teste para o controller

--- src/test/java/br/com/magazineluiza/wishlist/controllers/ClienteControllerTest.java
+++ src/test/java/br/com/magazineluiza/wishlist/controllers/ClienteControllerTest.java
@@ -3,20 +3,51 @@ package br.com.magazineluiza.wishlist.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.magazineluiza.wishlist.domain.entity.Cliente;
+import br.com.magazineluiza.wishlist.domain.entity.ClienteBuilder;
 import br.com.magazineluiza.wishlist.controllers.ClienteController;
 import br.com.magazineluiza.wishlist.domain.service.ClienteService;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class ClienteControllerTest {
   @Autowired
   protected ClienteController clienteController;
 
+  @MockBean
+	private ClienteService service;
+
   @Autowired
-  protected ClienteService clienteService;
+  protected MockMvc mockMvc;
+
   
+  @Test
+	public void createShouldReturnCliente() throws Exception {
+        Cliente cliente = new ClienteBuilder().defaultValues();
+            when(service.Create(cliente)).thenReturn(cliente);
 
-  @Test 
-  public void cadastro(){
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/cliente")
+                  .contentType(MediaType.APPLICATION_JSON_VALUE)
+                  .content(asJsonString(cliente)))
+                  .andExpect(MockMvcResultMatchers.status().isOk());
+	}
 
-  }
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/br/com/magazineluiza/wishlist/domain/entity/ClienteTest.java
+++ src/test/java/br/com/magazineluiza/wishlist/domain/entity/ClienteTest.java
@@ -1,16 +1,21 @@
 package com.magazineluiza.wishlist.domain.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.magazineluiza.wishlist.domain.entity.Cliente;
+import br.com.magazineluiza.wishlist.domain.entity.ClienteBuilder;
 
 @SpringBootTest
 public class ClienteTest {
 
    @Test
     public void criandoInstanciaCliente(){
-        Cliente cliente = new CarroBuilder().defaultValues();
+        Cliente cliente = new ClienteBuilder().defaultValues();
         assertThat(cliente).isNotNull();
     }
 
