b550197c10665a2bed2bd4d830ff968596b1470d
Alessandra Silva <silvaalejesus@gmail.com>
2021-05-15 14:26:11 -0300

    feat: atualizado

--- src/main/java/br/com/magazineluiza/wishlist/domain/repository/ClienteRepository.java
+++ src/main/java/br/com/magazineluiza/wishlist/domain/repository/ClienteRepository.java
@@ -3,6 +3,7 @@ package br.com.magazineluiza.wishlist.domain.repository;
 import org.springframework.data.repository.CrudRepository;
 import br.com.magazineluiza.wishlist.domain.entity.Cliente;
 
+@Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-
+    public List<Cliente> findByName(String nome);
 }
--- src/main/java/br/com/magazineluiza/wishlist/domain/repository/ProdutoRepository.java
+++ src/main/java/br/com/magazineluiza/wishlist/domain/repository/ProdutoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.magazineluiza.wishlist.domain.entity.Produto;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Long> {
-
+    public List<Produto> findByName(String nome);
 }
 
