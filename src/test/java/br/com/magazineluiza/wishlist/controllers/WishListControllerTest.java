054627d54f7ac708e54fd56b9294bd01e1cbc80b
Vanessa Santana <vanessah4money@gmail.com>
2021-05-20 00:23:30 -0300

    test: contoller wishlist

--- src/test/java/br/com/magazineluiza/wishlist/controllers/WishListControllerTest.java
+++ src/test/java/br/com/magazineluiza/wishlist/controllers/WishListControllerTest.java
@@ -3,6 +3,7 @@ package br.com.magazineluiza.wishlist.controllers;
 import br.com.magazineluiza.wishlist.BaseTest;
 import br.com.magazineluiza.wishlist.domain.entity.Cliente;
 import br.com.magazineluiza.wishlist.domain.entity.ClienteBuilder;
+import br.com.magazineluiza.wishlist.domain.entity.Produto;
 import br.com.magazineluiza.wishlist.domain.entity.ProdutoBuilder;
 import br.com.magazineluiza.wishlist.domain.service.WishlistService;
 import org.junit.jupiter.api.DisplayName;
@@ -14,9 +15,13 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class WishListControllerTest extends BaseTest {
     @InjectMocks
@@ -34,24 +39,58 @@ public class WishListControllerTest extends BaseTest {
     @Test
     @DisplayName("Test Create Wishlist Return Success")
     public void CreateClienteReturnSuccess() throws Exception {
-        String uri = "/wishList";
+        String uri = "/wishList/";
         Cliente cliente = clienteBuilder.defaultValues();
-//
-//        when(_wishlistService.Create(isA(Long.class)))
-//                .thenReturn(cliente);
-//
-//        String inputJson = super.mapToJson(cliente);
-//        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders.post(uri)
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(inputJson))
-//                .andReturn()
-//                .getResponse();
-//
-//        assertEquals(response.getStatus(), HttpStatus.CREATED.value());
-//        Cliente clienteResponse = mapFromJson(response.getContentAsString(), Cliente.class);
-//        assertEquals(clienteResponse.getId(), cliente.getId());
-//        assertEquals(clienteResponse.getNome(), cliente.getNome());
-//        assertEquals(clienteResponse.getSobrenome(), cliente.getSobrenome());
-//        assertEquals(clienteResponse.getCpf(), cliente.getCpf());
+        Produto produto = produtoBuilder.defaultValues();
+        Set<Long> idProduto = new HashSet<>();
+        idProduto.add(1L);
+
+        MockHttpServletResponse response =  (MockHttpServletResponse) mvc.perform(MockMvcRequestBuilders.post(uri,cliente.getId(),idProduto))
+                .andExpect(status().isCreated())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test Create GetProdutosByIdCliente Return Success")
+    public void GetProdutosByIdClienteReturnSuccess() throws Exception {
+        String uri = "/wishList/cliente/";
+        Cliente cliente = clienteBuilder.defaultValues();
+        Produto produto = produtoBuilder.defaultValues();
+        Set<Produto> listaProdutos = new HashSet<>();
+        listaProdutos.add(produto);
+        when(_wishlistService.GetProdutosByIdCliente(isA(Long.class)))
+                .thenReturn(listaProdutos);
+
+        MockHttpServletResponse response =  (MockHttpServletResponse) mvc.perform(MockMvcRequestBuilders.get(uri,cliente.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test Create GetProdutosByIdCliente Return Success")
+    public void GetProdutosByDeleteReturnSuccess() throws Exception {
+        String uri = "wishList/cliente/1/produto/1";
+        Cliente cliente = clienteBuilder.defaultValues();
+        Produto produto = produtoBuilder.defaultValues();
+        when(_wishlistService.Delete(isA(Long.class),isA(Long.class)))
+                .thenReturn(true);
+
+        MockHttpServletResponse response =  (MockHttpServletResponse) mvc.perform(MockMvcRequestBuilders.delete(uri))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test Insert Return Success")
+    public void InsertReturnSuccess() throws Exception {
+        String uri = "wishList/cliente/1/produto/1";
+        Cliente cliente = clienteBuilder.defaultValues();
+        Produto produto = produtoBuilder.defaultValues();
+        when(_wishlistService.Insert(isA(Long.class),isA(Long.class)))
+                .thenReturn(true);
+
+        MockHttpServletResponse response =  (MockHttpServletResponse) mvc.perform(MockMvcRequestBuilders.post(uri))
+                .andExpect(status().isCreated())
+                .andReturn();
     }
 }
